4525774f80df372d1e4e43d3124df8ce68701b17
Karan Kaushik <kkricardokaka95@gmail.com>
2020-05-09 00:08:00 +1000

    Working bot woohoo

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import token from 'token.js';
+const { prefix, token } = require('./config.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
-client.login(token);
+client.login(token); 
+
+
+client.on('message', message => {
+    if (message.content === '!help') {
+        message.channel.send('I am a bot for assisting you with Rainbow 6 Siege. Use the following commands to ask me stuff!');
+    }
+});
---
+++ token.js
@@ -0,0 +1,2 @@
+const token = 'NzA4MzA5MTg3MDg3ODI3MDQ1.XrVmCg.pYCmXbfrFzwPXr9v3DEU8fBlEAE';
+exports.token = token;
