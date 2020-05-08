26d6f103db8d650457a110c3c08fcee671dad1f9
Karan Kaushik <kkricardokaka95@gmail.com>
2020-05-09 00:14:51 +1000

    Working again, token is no longer public

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./config.json'); // config.json is gitignored because privacy and token
 
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // Discord JS provides the boilerplate for messing around with Discord and discord servers
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Ready!'); // When the node application has started successfully, this'll log on the console
 });
 
-client.login(token); 
+client.login(token); //Login to the discord app using our super secret token
 
-
+// The main "menu"
 client.on('message', message => {
-    if (message.content === '!help') {
+    if (message.content === `${prefix}help`) {
         message.channel.send('I am a bot for assisting you with Rainbow 6 Siege. Use the following commands to ask me stuff!');
     }
 });
